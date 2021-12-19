64771145a907d3d2c2dbbe953fe8a97df075222b
simonjujuh <juhel.simon6@gmail.com>
2021-12-19 18:50:08 +0100

    Video 2

--- flask_blog.py
+++ flask_blog.py
@@ -1,15 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Simon',
+        'title': 'Blog post one',
+        'content': 'First post content',
+        'dateposted': 'April 21 2021',
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog post two',
+        'content': 'Second post content',
+        'dateposted': 'April 22 2021',
+    },
+]
 
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return "<h1>Home page</h1>"
+    return render_template('home.html', posts=posts)
+
 
 @app.route('/about')
 def about():
-    return "Flask blog version 0.1-alpha release"
+    return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
