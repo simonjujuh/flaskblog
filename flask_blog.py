54af7def568c0139e6405f3a749484f33cbfa87f
simonjujuh <juhel.simon6@gmail.com>
2021-12-20 23:34:18 +0100

    Video 3

--- flask_blog.py
+++ flask_blog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '01e631fc85e74176f3a47bc437fe9ad2'
 
 posts = [
     {
@@ -20,13 +22,31 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login successfully! Welcome back {form.email.data}", "info")
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
